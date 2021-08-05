8b5a51733583b990e636fc8e0c1ce422419e1636
Sepanta <Sepanta_97@Yahoo.com>
2021-08-05 19:33:56 +0430

    useState_&_useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputChangeHandler = (event) => setEnteredName(event.target.value);
+  const inpRef = useRef();
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const RefVal = inpRef.current.value;
+
+    console.log(enteredName);
+    console.log(RefVal);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={inpRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
